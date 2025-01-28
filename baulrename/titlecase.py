f1f744137e58b7be0e6383be40a2cd5cefb3019a
Pablo Barciela <scow@riseup.net>
2025-01-28 14:12:36 +0100

    titlecase: avoid invalid escape sequence
    Fixes the warning:
    usr/local/lib/python3.13/dist-packages/baulrename/titlecase.py:17: SyntaxWarning: invalid escape sequence '\.'

--- baulrename/titlecase.py
+++ baulrename/titlecase.py
@@ -14,7 +14,7 @@ import sys
 __all__ = ['titlecase']
 __version__ = '0.9.0'
 
-SMALL = 'a|an|and|as|at|but|by|en|for|if|in|of|on|or|the|to|v\.?|via|vs\.?'
+SMALL = 'a|an|and|as|at|but|by|en|for|if|in|of|on|or|the|to|v.?|via|vs.?'
 PUNCT = r"""!"#$%&'‘()*+,\-./:;?@[\\\]_`{|}~"""
 
 SMALL_WORDS = re.compile(r'^(%s)$' % SMALL, re.I)
