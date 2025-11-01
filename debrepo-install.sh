cb583f70dffdcc5b3558989d01cdb1217c1e3fd6
Pablo Barciela <scow@riseup.net>
2025-11-01 23:18:06 +0100

    ci: test ci

---
+++ debrepo-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -v
+set -x
+
+# Install .deb packages from release branch
+echo deb [trusted=yes] https://cafe-desktop.github.io/debian-packages/ ./ >> /etc/apt/sources.list
+aptitude -y update
+aptitude -y update
+
+if [ $# -gt 0 ]
+then
+  aptitude -y install $1 > aptlog
+  if grep -w "unmet dependencies" aptlog; then
+    cat aptlog
+    exit 1
+  fi
+  rm aptlog
+fi
